acc363b882757aa08f9a7e5b998d119fdc472ebf
randyer <rbdyer3@gmail.com>
2024-09-25 21:22:48 -0400

    hero screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,33 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-700">
-        Hello world!
-      </h1>
+      <nav className="fixed top-0 flex justify-between items-center w-full p-4 bg-black">
+        <h1 className="text-white">Alton Therapeutic Massage</h1>
+        <ul className="flex justify-between items-center gap-x-4 text-white">
+          <li>
+            <a href="#Treatments">Treatments</a>
+          </li>
+          <li>
+            <a href="#Expectations">Expectations</a>
+          </li>
+          <li>
+            <a href="#Reviews">Reviews</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact</a>
+          </li>
+          <li>
+            <a href="#About">About</a>
+          </li>
+          <li>
+            <a href="#Qualifications">Qualifications</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex flex-col justify-center items-center header w-full h-screen bg-black text-white">
+        <h1>Craniosacral Therapy</h1>
+        <h2>Healing through gentle touch</h2>
+      </div>
     </>
   );
 }
